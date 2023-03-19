f01a8353fdbdf2302d1439014c38fd0d649c7854
phong25t25t <phong25t25t@gmail.com>
2023-03-19 17:52:15 +0700

    #01 rollback commit

--- public/js/step3.js
+++ public/js/step3.js
@@ -62,27 +62,24 @@ $(document).ready(function () {
     $('#add-exp-btn').click( function( event ) {
         event.preventDefault();
         if (validateExp('add')) {
-            var arr = {
+            let experience = {
                 'title' :  $('#exp-title').val(),
-                'company' : $('#exp-company').val(),
+                'company_name' : $('#exp-company').val(),
                 'location' : $('#exp-location').val(),
-                'type' : $('#exp-type').val(),
+                'employment_type' : $('#exp-type').val(),
                 'start_date' : $('#exp-start-date').val(),
                 'end_date' : $('#exp-end-date').val(),
             };
 
-            experience[arr.company] = arr;
-            renderExpDiv(arr);
-            $('.box-lightbox').removeClass('open');
-            returnModal();
+            ajaxRenderCard('experience', experience);
         }
     });
 
     function returnModal() {
         var all_input = document.querySelectorAll(".model-body input");
-        for (exp_input of all_input) {
-            exp_input.value = '';
-            exp_input.classList.remove("warning");
+        for (p_input of all_input) {
+            p_input.value = '';
+            p_input.classList.remove("warning");
         }
     }
 
@@ -146,6 +143,7 @@ $(document).ready(function () {
 
     //------------Education Field-----------//
     const eduCol = $('.col-education');
+
     $('#add-education').click( function( event ) {
         const eduInput = document.createElement('input');
         eduInput.setAttribute('class', 'field');
@@ -167,10 +165,10 @@ $(document).ready(function () {
         $('.add-certification-modal').addClass('open');
     });
 
-    $('#add-cert-btn').click( function( event ) {
+    $('#add-cert-card').click( function( event ) {
         event.preventDefault();
         if (validateCert('add')) {
-            var arr = {
+            var certification = {
                 'name' :  $('#cert-name').val(),
                 'organization' : $('#cert-organization').val(),
                 'issue_date' : $('#cert-issue-date').val(),
@@ -179,56 +177,29 @@ $(document).ready(function () {
                 'credential_url' : $('#cert-credential-url').val(),
             };
 
-            certificate[arr.name] = arr;
-            renderCertDiv(arr);
-            $('.box-lightbox').removeClass('open');
-            returnModal();
+            ajaxRenderCard('certification', certification);
         }
     });
-
-    function renderCertDiv(cert = []) {
-        let diff_month = monthDiff(new Date(cert.issue_date), new Date(cert.expiration_date));
-        if (cert.credential_id != '') {
-            cert.credential_id = "Credintial ID " + cert.credential_id;
-        }
-        // render
-        const card = document.createElement('div');
-        card.setAttribute('class', 'form-certification-card');
-        card.innerHTML =
-            `<div><img src="" alt="" width="48"></div>
-            <div class="certification-card">
-                <div class="certification-name"><span>` + cert.name + `</span></div>
-                <span class="certification-organization">` + cert.organization + `</span>
-                <span class="certification-info"> Issued ` + cert.issue_date + `</span>
-                <span class="certification-info">` + cert.credential_id + `</span>
-            </div>
-            <div class="button-wrapper">
-                <div class="swiper-button swiper-next-button edit-card-cert" role="button" data-test="`+ cert.name +`">
-                    <svg viewBox="0 0 15 15" fill="none" stroke="currentColor"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" /> </svg>
-                </div>
-            </div>`;
-        certDiv.append(card);
-    }
 
     function validateCert(action) {
         var all_input = document.querySelectorAll(`#${action}-certification input`),
             check_issue_date,
             check_expiration_date,
             check = true;
-        for (exp_input of all_input) {
-            if (exp_input.id == "cert-credential-id") {
+        for (cert_input of all_input) {
+            if (cert_input.id == "cert-credential-id") {
                 continue;
             }
-            if (exp_input.value.length <= 1) {
-                exp_input.classList.add("warning");
+            if (cert_input.value.length <= 1) {
+                cert_input.classList.add("warning");
                 check = false;
             } else {
-                exp_input.classList.remove("warning");
-                if (exp_input.id == "cert-issue-date") {
-                    check_issue_date = exp_input;
+                cert_input.classList.remove("warning");
+                if (cert_input.id == "cert-issue-date") {
+                    check_issue_date = cert_input;
                 }
-                if (exp_input.id == "cert-expiration-date") {
-                    check_expiration_date = exp_input;
+                if (cert_input.id == "cert-expiration-date") {
+                    check_expiration_date = cert_input;
                 }
                 if (check_issue_date && check_expiration_date) {
                     let month_diff = monthDiff(new Date(check_issue_date.value), new Date(check_expiration_date.value));
@@ -243,6 +214,28 @@ $(document).ready(function () {
             return false;
         }
         return true;
+    }
+
+
+    function ajaxRenderCard(name, data) {
+        $.ajax({
+            type: "POST",
+            url: '/api/step/render-' + name,
+            data: { data },
+            dataType: 'json',
+            success: function (response) {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'form-' + name + '-card');
+                card.innerHTML = response.data;
+                certDiv.append(card);
+
+                $('.box-lightbox').removeClass('open');
+                returnModal();
+            },
+            error: function (response) {
+                alert(response.responseJSON.message);
+            }
+        });
     }
 });
 
