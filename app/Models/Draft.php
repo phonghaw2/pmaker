f19002598196a9a739fe594cf9c7264804aeb0cb
phong25t25t <phong25t25t@gmail.com>
2023-05-17 23:20:38 +0700

    #05 test model, check save

---
+++ app/Models/Draft.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Draft extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'context',
+        'draft_id',
+    ];
+}
