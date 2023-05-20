05405b1e1d95bdbe74343cdaefd90e757002a9bd
phong25t25t <phong25t25t@gmail.com>
2023-05-20 23:00:15 +0700

    Add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,9 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'alreadyLoggedIn' => \App\Http\Middleware\AlreadyLoggedIn::class,
+        'hasLogged' => \App\Http\Middleware\AuthCheck::class,
+        'isAdmin' => \App\Http\Middleware\AdminCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
