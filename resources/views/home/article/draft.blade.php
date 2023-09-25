3ab99ac178977584d3fcdc69220410dfc0dbc0dd
phong25t25t <phong25t25t@gmail.com>
2023-09-25 21:36:39 +0700

    #08 Update: html magic tool

--- resources/views/home/article/draft.blade.php
+++ resources/views/home/article/draft.blade.php
@@ -162,39 +162,56 @@
                     <div class="inner">
                         <div class="menu-float__inner">
                             <div class="menu-float__wrapper">
-                                <div class="menu-float__content">
-                                    <ul class="menu-float__filters">
-                                        <li>
-                                            <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
-                                            Awards
-                                            <svg class="ico-svg menu-float__arrow" viewBox="0 0 20 20" width="12"><use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#arrow-dd"></use></svg>
-                                            </span>
-                                        </li>
-                                        <li>
-                                            <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
-                                            Awards
-                                            <svg class="ico-svg menu-float__arrow" viewBox="0 0 20 20" width="12"><use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#arrow-dd"></use></svg>
-                                            </span>
-                                        </li>
-                                        <li>
-                                            <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
-                                            Awards
-                                            <svg class="ico-svg menu-float__arrow" viewBox="0 0 20 20" width="12"><use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#arrow-dd"></use></svg>
-                                            </span>
-                                        </li>
-                                        <li>
-                                            <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
-                                            Awards
-                                            <svg class="ico-svg menu-float__arrow" viewBox="0 0 20 20" width="12"><use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#arrow-dd"></use></svg>
-                                            </span>
-                                        </li>
-                                        <li>
-                                            <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
-                                            Awards
-                                            <svg class="ico-svg menu-float__arrow" viewBox="0 0 20 20" width="12"><use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#arrow-dd"></use></svg>
-                                            </span>
-                                        </li>
-                                    </ul>
+                                <div class="menu-float__top">
+                                    <div class="menu-float__menu" id="menu-filters" style="display: block;">
+                                        <div class="menu-float__menu-content">
+                                            <div class="filters">
+                                                <div class="filters__tab" id="filter-winners-popular" data-controller="searchable-filter" style="display: block;">
+                                                    <div class="filters__search">
+                                                        <input type="text" class="js-input-filter-search" placeholder="Search ..." data-searchable-filter-target="input" data-action="keyup->searchable-filter#searchEvent">
+                                                        <button type="submit" data-action="click->searchable-filter#doSearch">
+                                                            <svg class="ico-svg" viewBox="0 0 20 20" width="17">
+                                                                <use xlink:href="https://www.awwwards.com/assets/redesign/images/sprite-icons.svg#lupe"></use>
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                    <div class="filters__content" data-searchable-filter-target="filters">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="menu-float__bottom">
+                                    <div class="menu-float__content">
+                                        <strong class="menu-float__title-section">PMaker</strong>
+                                        <ul class="menu-float__filters">
+                                            <li>
+                                                <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
+                                                Tags
+                                                <svg style="width:12px;height:12px;fill:#a7a7a" class="menu-float__arrow" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
+                                                Series
+                                                <svg style="width:12px;height:12px;fill:#a7a7a" class="menu-float__arrow" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
+                                                Awards
+                                                <svg style="width:12px;height:12px;fill:#a7a7a" class="menu-float__arrow" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                                                </span>
+                                            </li>
+                                            <li>
+                                                <span class="menu-float__filter js-menufloat-filter" data-id="filter-winners-award">
+                                                Awards
+                                                <svg style="width:12px;height:12px;fill:#a7a7a" class="menu-float__arrow" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                                                </span>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
